cc1205a50ae37432e1afb93f9306a0c2bd9e828d
Amber Mattis <amber.mattis2@gmail.com>
2021-01-12 15:04:43 -0600

    created code to find all employees hired in teh 90s

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,4 +2,9 @@ USE employees;
 
 SELECT first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'VIdya', 'Maya')
+WHERE first_name IN ('Irena', 'VIdya', 'Maya');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE'E%';
+
