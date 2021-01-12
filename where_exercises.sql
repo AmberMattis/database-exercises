1bc5bcf91c420bef642ca988b226414ff2faf717
Amber Mattis <amber.mattis2@gmail.com>
2021-01-12 15:11:17 -0600

    wrote code to find all emplyees with q in their last name

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,3 +15,7 @@ WHERE hire_date LIKE '199%';
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%12-25';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
