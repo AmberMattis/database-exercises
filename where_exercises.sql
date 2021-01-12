5a25f4de5d6e79d67be488a709bbdf649c2e0f3f
Amber Mattis <amber.mattis2@gmail.com>
2021-01-12 15:07:07 -0600

    created code to find all employees hired on Christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,3 +8,10 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE'E%';
 
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '199%';
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%12-25';
