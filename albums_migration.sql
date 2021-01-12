a8be72841b3f4464d4b52663998a7b45580c47a8
Amber Mattis <amber.mattis2@gmail.com>
2021-01-12 13:59:15 -0600

    created albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,14 @@
 USE codeup_test_db;
-DROP TABLE IF EXISTS albums;
-CREATE TABLE albums(
-     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     artist VARCHAR(150) NOT NULL,
-     name VARCHAR(150) NOT NULL,
-     release_date INT(4),
-     sales DOUBLE(4),
-     genre VARCHAR(150),
- );
+
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name VARCHAR (100),
+    release_date INT,
+    sales DOUBLE,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+);
+
+
