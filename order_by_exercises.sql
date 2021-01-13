6c840db73c725e20264c3a25f65a8893a63d4f6d
Amber Mattis <amber.mattis2@gmail.com>
2021-01-13 11:02:55 -0600

    refactored code to display 90s hires born on Christmas in descending order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -19,12 +19,23 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name;
 
 # 5. refactored to order all employees with 'e' in their last name by emplyee number
-SELECT first_name, last_name
+SELECT first_name, last_name, emp_no
 FROM employees
-WHERE last_name LIKE'E%'
+WHERE last_name LIKE'%E%'
 ORDER BY emp_no;
 
+# 6. refactored to reverse the order of employees by employee number amd last name
+SELECT first_name, last_name, emp_no
+FROM employees
+WHERE last_name LIKE'%E%'
+ORDER BY emp_no DESC, last_name DESC;
 
+# 7. refactored Christmas hires born in the 90s so first result is Khun Bernini oldest employee
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE  hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%12-25'
+ORDER BY hire_date DESC;
 
 
 # Refactored using Or instead of IN
@@ -71,8 +82,10 @@ WHERE last_name LIKE '%E'
 # Find all employees born Christmas and hired in the 90s
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE  hire_date LIKE '199%'
-  AND hire_date LIKE '%12-25';
+WHERE  hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%12-25';
+
+
 
 # Find all employees with 'q' but not 'qu' in last name
 SELECT first_name, last_name
