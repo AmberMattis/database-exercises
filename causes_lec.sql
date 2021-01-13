f3991077d64c8293a3a0b612f03a0b6049fba6fe
Amber Mattis <amber.mattis2@gmail.com>
2021-01-12 19:36:58 -0600

    added comments

--- causes_lec.sql
+++ causes_lec.sql
@@ -28,3 +28,4 @@ FROM employees
 WHERE emp_no < 20000
     AND  last_name IN ('Herber','Baek')
      OR first_name = 'Shridhar';
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,5 +1,6 @@
 USE employees;
 
+# find all employees with these first names
 SELECT first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
@@ -13,23 +14,27 @@ WHERE gender = 'M'
    OR first_name LIKE 'Vidya');
 
 
-
+# Find employees with last names that contain an 'e'
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE'E%';
 
+# Find all employees hire in the 90s.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '199%';
 
+# Find all employees hired on Christmas
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%12-25';
 
+# Find all employees with a last name tha contains a 'q'.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
 
+# Find all employees with a last name that starts and ends with 'e'.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%E'
@@ -45,4 +50,10 @@ WHERE last_name LIKE '%E'
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE  hire_date LIKE '199%'
-AND hire_date LIKE '%12-25'
+AND hire_date LIKE '%12-25';
+
+# Find all employees with 'q' but not 'qu' in last name
+SELECT first_name, last_name
+FROM employees
+WHERE last_name NOT LIKE '%qu%'
+  AND last_name LIKE '%q%';
