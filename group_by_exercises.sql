ecb2cbcc6d79f39584f73aeb4eb3cec63ce394ba
Amber Mattis <amber.mattis2@gmail.com>
2021-01-14 10:43:38 -0600

    refactored code for last names that start and end with 'e' to display only the unique last names

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,8 +1,14 @@
 USE employees;
 
-# used distinct to find all the distinct job titles in the titles tables
+# 2 used distinct to find all the distinct job titles in the titles tables
 SELECT DISTINCT title
 FROM titles
 GROUP BY title;
 
 
+# 3 refactored code for last names that start and end with 'e' to display only the unique last names
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%E'
+  AND last_name LIKE 'E%'
+GROUP BY last_name;
