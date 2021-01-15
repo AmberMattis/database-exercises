97c8f3e5c83ced6c48f8b369042629eb17be100b
Amber Mattis <amber.mattis2@gmail.com>
2021-01-14 19:30:01 -0600

    added updates to update exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,10 +1,35 @@
 USE codeup_test_db;
 
 # Will return all albums under collum title 'All albums in your table
-SELECT name AS 'All albums in your table'
+SELECT name, sales AS 'All albums in your table'
 FROM albums;
 
+SELECT * FROM albums;
+
+UPDATE albums
+SET sales = (sales * 10);
+
+SELECT * FROM albums;
+
+
 # Will return all albums that were released before 1980
-SELECT name AS 'All albums released before 1980'
+SELECT name, release_date AS 'All albums released before 1980'
 FROM albums
 WHERE release_date < 1980;
+
+UPDATE albums
+SET release_date = (release_date - 100);
+
+SELECT * FROM albums;
+
+# Will return all albums that were released by Michael Jackson
+SELECT artist, name AS 'Albums by Michael Jackson'
+FROM albums
+WHERE artist LIKE '%Michael Jackson%';
+
+UPDATE albums
+SET artist = 'Peter Jackson';
+
+SELECT * FROM albums;
+
+
