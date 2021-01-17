34bec7f23b5f832fe0d49190cbbdbbfce2cbda39
Amber Mattis <amber.mattis2@gmail.com>
2021-01-17 07:35:47 -0600

    wrote code to display all albums by Whitney Houston

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -5,9 +5,15 @@ SELECT * FROM albums
 WHERE release_date > 1991;
 
 
+
 # Will display all albums in the disco genre
 SELECT * FROM albums
 WHERE genre LIKE 'disco';
 
+
+
+# Will display all albums by Whitney Houston
+SELECT * FROM albums
+WHERE artist LIKE '%Whitney Houston%';
 
 
