27e069293f8a41588220c5c2f13f9d79c9294971
Amber Mattis <amber.mattis2@gmail.com>
2021-01-17 08:05:40 -0600

    refactored exercise 3 to order list by department name

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
-# Will display each department and the current manager of that department
+# 2. Will display each department and the current manager of that department
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS DEPARTMENT_MANAGER, d.dept_name AS DEPARTMENT_NAME
 FROM employees as e
          JOIN dept_manager as dm
@@ -11,11 +11,14 @@ WHERE dm.to_date = '9999-01-01'
 ORDER BY dept_name;
 
 
+
+# 3. Will display all departments currently managed by women
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS MANAGER_NAME, d.dept_name AS DEPARTMENT_NAME, gender
 FROM employees as e
     JOIN dept_manager as dm
         ON e.emp_no = dm.emp_no
     JOIN departments as d
         ON dm.dept_no = d.dept_no
-WHERE dm.to_date = '9999-01-01' AND gender LIKE ('F');
+WHERE dm.to_date = '9999-01-01' AND gender LIKE ('F')
+ORDER BY dept_name;
 
