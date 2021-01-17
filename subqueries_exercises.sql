f328e161a2d06bd3204b5533905d3cac9f71ad6d
Amber Mattis <amber.mattis2@gmail.com>
2021-01-17 09:37:31 -0600

    wrote code to find all titles held by employees named Aamod

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -9,3 +9,13 @@ WHERE hire_date IN (
     FROM employees
     WHERE emp_no LIKE 101010
     );
+
+# Find all titles held by all employees with the first name Aamod
+SELECT title, COUNT(title) AS Count
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name LIKE 'Aamod'
+)
+ GROUP BY title;
