a64c2ea312ce439ad77dc536effb9686f3b0b050
Amber Mattis <amber.mattis2@gmail.com>
2021-01-17 07:40:34 -0600

    wrote code to remove all albums in the disco genre

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,12 +1,35 @@
 USE codeup_test_db;
 
-# Will display all albums released after 1991
-SELECT * FROM albums
+# # Will display all albums released after 1991
+# SELECT * FROM albums
+# WHERE release_date > 1991;
+#
+#
+#
+# # Will display all albums in the disco genre
+# SELECT * FROM albums
+# WHERE genre LIKE 'disco';
+#
+#
+#
+# # Will display all albums by Whitney Houston
+# SELECT * FROM albums
+# WHERE artist LIKE '%Whitney Houston%';
+
+
+
+# Will Delete all albums released after 1991
+DELETE FROM albums
 WHERE release_date > 1991;
 
+SELECT release_date FROM albums;
 
 
-# Will display all albums in the disco genre
+
+# Will delete all albums in the disco genre
+DELETE FROM albums
+WHERE genre LIKE 'disco';
+
 SELECT * FROM albums
 WHERE genre LIKE 'disco';
 
@@ -15,5 +38,3 @@ WHERE genre LIKE 'disco';
 # Will display all albums by Whitney Houston
 SELECT * FROM albums
 WHERE artist LIKE '%Whitney Houston%';
-
-
