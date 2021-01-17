6b6d9a7e0142ada039c9ae48c608714fbff718f8
Amber Mattis <amber.mattis2@gmail.com>
2021-01-17 07:53:54 -0600

    added additional comments

--- indexes_exercises.sql
+++ indexes_exercises.sql
@@ -3,6 +3,7 @@ USE codeup_test_db;
 
 # SHOW TABLES;
 
+# 2. Will describe and inspect the keys of each table
 # DESCRIBE employees;
 # DESCRIBE dept_emp;
 # DESCRIBE dept_manager;
@@ -11,12 +12,12 @@ USE codeup_test_db;
 # DESCRIBE departments;
 
 
+# Provide a unique key for artist and name combination
 ALTER TABLE albums
 ADD UNIQUE unique_nm_ar (artist,name);
 
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Jacob Collier', 'Djesse Vol. 3', '2020', '1.5','RnB');
-
 
 SHOW TABLES;
 
